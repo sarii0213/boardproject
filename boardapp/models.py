d7432b23229e9b9e7b5b8a319a9138656da20823
sarii0213 <anjing.singka@gmail.com>
2024-01-07 10:12:42 +0900

    first commit

---
+++ boardapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class BoardModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    sns_image = models.ImageField(upload_to='')
+    good = models.IntegerField(null=True, blank=True, default=0)
+    read = models.IntegerField(null=True, blank=True, default=1)
+    read_text = models.TextField(null=True, blank=True, default='')
+
+    def __str__(self):
+        return self.title
